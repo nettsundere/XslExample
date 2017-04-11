eca877c42c4933338069c6509b48353f3f2e28e4
Vladimir nettsundere Kiselev <nettsundere@gmail.com>
2017-04-12 01:43:14 +0300

    Add an example

---
+++ XmlApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Xsl;
+
+namespace XmlApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xslt = new XslCompiledTransform(true);
+
+            xslt.Load("lyricsTemplate.xsl");
+
+            xslt.Transform("lyrics.xml", "output.xml");
+        }
+    }
+}
---
+++ XmlApp/lyricsTemplate.xsl
@@ -0,0 +1,16 @@
+﻿<?xml version="1.0"?>
+
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <xsl:template match="lines/*">
+    <xsl:value-of select="local-name(.)" />
+    are
+    <xsl:value-of select="./@color" />
+    <xsl:value-of select="' '"/>
+  </xsl:template>
+
+  <xsl:template match="/">
+    <poem>
+      <xsl:apply-templates select="/lines/*" />
+    </poem>
+  </xsl:template>
+</xsl:stylesheet>
